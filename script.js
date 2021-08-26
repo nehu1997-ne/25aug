3beb93193b34a06d2c6cf97e1562d757c6c9f98b
Neha Singh <69458966+nehu1997-ne@users.noreply.github.com>
2021-08-26 13:10:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+// document.getElementById("frm").addEventListener("submit", function(){
+//     event.preventDefault()
+//     // var data= serialize(this)
+//     // console.log(data)
+
+
+// // function serialize(frm){
+//     var pets={}
+// //     var obj= new FormData(frm)
+// //     obj.forEach((e,k)=>{
+// //          console.log(e,k)
+// //         pets[k]=e
+// //     })
+// //     return pets
+// //     }
+// var obj= new FormData(this)
+// obj.forEach((e,k)=>{
+//     console.log(e,k)
+//     pets[k]=e
+// })
+// console.log(pets)
+// })
+
+// // function loadpets(){
+
+
+// // var http=new XMLHttpRequest()
+// // http.open("GET","pets.json")
+// // http.send()
+// // http.onreadystatechange=function(){
+// //     if(this.readyState==4 && this.status==200)
+// // {
+// //     var result= JSON.parse(this.response)
+// //     console.log(result)
+// // }
+// // }
+
+
+
+
+
+// // fetch("pets.json").then(function(res){
+// //             if(res.ok){
+// //                 res.json().then(function(result){
+// //                     console.log(result)
+// //                 })
+// //             }
+
+// //     })
+// // }
+// // loadpets()
+//  document.getElementById("frm").addEventListener("submit", function(){
+//     event.preventDefault()
+//      var obj=new FormData(this)
+//      console.log((e)=>{
+//          console.log(e)
+//     })
+//  })
+document.getElementById("frm").addEventListener("submit", function(){
+    alert("hello")
+})
